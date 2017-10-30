0b2ef1eeee732361980aff6e1a0fe943bb6ea8dd
Nolan Clark <nolanmclark@gmail.com>
2017-10-30 12:10:55 -0500

    Update auth.config.ts

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -4,6 +4,6 @@ interface AuthConfiguration {
 }
 
 export const myConfig: AuthConfiguration = {
-    clientID: 'HuIk02ExITh91ahIrfZnylge3kOMDbpx',
-    domain: 'nolanclark.auth0.com'
+    clientID: '',
+    domain: ''
 }
